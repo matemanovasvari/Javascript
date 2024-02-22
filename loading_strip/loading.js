77ab470ae6e1968b4bb2b71731601fe333de4dae
Nagy Máté <nagy.mano.mate@gmail.com>
2024-02-22 10:07:26 +0100

    sssssss

--- loading_strip/loading.js
+++ loading_strip/loading.js
@@ -4,15 +4,18 @@ var cucc = 0;
 function Start(){
 loader = setInterval(function()
 {
-    document.getElementById("load").style.width = `${cucc}%`
+    document.getElementById("loading_msg").style.visibility = "visible";
+    document.getElementById("load").style.width = `${cucc}%`;
     cucc += 1;  
     if(cucc == 100){
-        clearInterval(loader)
+        clearInterval(loader);
+        document.getElementById("loading_msg").style.visibility = "hidden";
+        document.getElementById("msg").style.visibility = "visible";
     }
     
 }, 10)
 }
 
 function Clear(){
-    window.location.reload()
+    window.location.reload();
 }
